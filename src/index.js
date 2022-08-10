3de838b0e08a9bc8a8ac3c953497b0561063e791
Xandowski <xandy-moraes@hotmail.com>
2022-08-10 03:12:09 -0300

    refactoring css styles

--- src/index.js
+++ src/index.js
@@ -1,56 +1,18 @@
 const themeSwitchBtn = document.querySelector('[data-js="theme-switch-btn"]')
 const moon = document.querySelector('[data-js="moon"]')
-const copyright = document.querySelector('[data-js="copyright"]')
 const sun = document.querySelector('[data-js="sun"]')
 const body = document.querySelector('body')
-const paragraphs = document.querySelectorAll('p')
-const detailsTitle = document.querySelectorAll('h3')
-const price = document.querySelectorAll('strong')
-const title = document.querySelector('h1')
-const footer = document.querySelector('footer')
+// const copyright = document.querySelector('[data-js="copyright"]')
+// const footer = document.querySelector('footer')
 const logoDark = document.querySelector('[data-js="logo-dark"]')
 const logo = document.querySelector('[data-js="logo"]')
 
 const switchTheme = () => {
-  if (!moon.classList.contains('_moon')) {
-    moon.classList.add('_moon')
-    sun.classList.add('_sun')
-    body.classList.add('_body')
-    paragraphs.forEach(p => {
-      p.classList.add('_text')
-      p.classList.add('_text_opacity')
-    })
-    detailsTitle.forEach(h3 => {
-      h3.classList.add('_text')
-    })
-    price.forEach(strong => {
-      strong.classList.add('_text')
-    })
-    title.classList.add('_text')
-    copyright.classList.add('_text')
-    footer.classList.add('_footer-border')
-    logoDark.classList.add('_logo-dark')
-    logo.classList.add('_logo')
-  } else {
-    moon.classList.remove('_moon')
-    sun.classList.remove('_sun')
-    body.classList.remove('_body')
-    paragraphs.forEach(p => {
-      p.classList.remove('_text')
-      p.classList.remove('_text_opacity')
-    })
-    detailsTitle.forEach(h3 => {
-      h3.classList.remove('_text')
-    })
-    price.forEach(strong => {
-      strong.classList.remove('_text')
-    })
-    title.classList.remove('_text')
-    copyright.classList.remove('_text')
-    footer.classList.remove('_footer-border')
-    logoDark.classList.remove('_logo-dark')
-    logo.classList.remove('_logo')
-  }
+  body.classList.toggle('dark_mode')
+  moon.classList.toggle('_moon')
+  sun.classList.toggle('_sun')
+  logo.classList.toggle('_logo')
+  logoDark.classList.toggle('_logo-dark')
 }
 
 themeSwitchBtn.addEventListener('click', switchTheme, false)
